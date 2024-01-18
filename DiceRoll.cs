5aa28835c5cb3928dd452a1300b2789d415707ef
Blake McAvoy <120762820+bamcavoy@users.noreply.github.com>
2024-01-17 23:54:43 -0700

    Add files via upload

---
+++ DiceRoll.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission02
+{
+    internal class DiceRoll
+    { 
+        // constructor
+        public DiceRoll() 
+        {
+        }
+
+        // roll one die
+        public int Roll()
+        {
+            Random dieRoll = new Random();
+
+            int roll = dieRoll.Next(1, 7);
+
+            return roll;
+        }
+
+        // roll 2 dice as many times as specified
+        public int[] RollAll(int diceCount)
+        {
+            // create results array
+            int[] rollCounts = new int[13];
+
+            for (int count = 0; count < diceCount; count++)
+            {
+                DiceRoll die1 = new DiceRoll();
+                DiceRoll die2 = new DiceRoll();
+
+                int roll1 = die1.Roll();
+                int roll2 = die2.Roll();
+                int rollTotal = roll1 + roll2;
+
+                // add to results array
+                rollCounts[rollTotal]++;
+            }
+
+            // return results array
+            return rollCounts;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// Blake McAvoy Section 2
+// Mission 02 assignment
+
+using Mission02;
+
+internal class Program
+{
+    // Main program class
+    private static void Main(string[] args)
+    {
+        // declare variable
+        int diceCount = 0;
+
+        // beginning prompts
+        System.Console.WriteLine("Welcome to the dice rolling simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        diceCount = int.Parse(System.Console.ReadLine());
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + diceCount + ".\n");
+
+        // instantiate DiceRoll
+        DiceRoll diceRoll = new DiceRoll();
+
+        // Store RollAll array results to rollCounts
+        int[] rollCounts = diceRoll.RollAll(diceCount);
+
+        // Build histogram for each possible result
+        for (int count = 2; count <= 12; count++) 
+        {
+            int rollPercent = (int)(((double)rollCounts[count] / diceCount) * 100);
+
+            string stars = BuildString(rollPercent);
+
+            System.Console.WriteLine((count) + ": " + stars);
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice rolling simulator. Goodbye!");
+    }
+
+    // method to add *'s equal to percent
+    private static string BuildString(int count)
+    {
+        string result = "";
+
+        for (int i = 0; i < count; i++)
+        {
+            result = result + "*";
+        }
+
+        return result;
+    }
+}
